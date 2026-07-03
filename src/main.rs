61f5005c2ff85becc0520647041ba750801532ee
Kaden Cyster <kadencyster@gmail.com>
2026-07-02 19:51:22 -0700

    removed old code for full rewrite of renderer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-mod engine;
-
-use engine::app::App;
-use winit::event_loop::{ControlFlow, EventLoop};
-
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app).unwrap();
+  println!("Hello World");
 }
